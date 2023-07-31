db331e047d3e685714cf92e23ac2763150bde2f3
acs0209 <annjae4676@naver.com>
2023-07-31 22:56:13 +0900

    [Init] - initializing

---
+++ back-end/ailion/src/main/java/back/ailion/AilionApplication.java
@@ -0,0 +1,13 @@
+package back.ailion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AilionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AilionApplication.class, args);
+	}
+
+}
---
+++ back-end/ailion/src/test/java/back/ailion/AilionApplicationTests.java
@@ -0,0 +1,13 @@
+package back.ailion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AilionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
