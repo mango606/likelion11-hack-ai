469716cd89d8753aba402af91a35711c94b2dc78
kcwww <cwkim0321@gmail.com>
2023-08-01 21:53:53 +0900

    [feat] -add birthday time

--- src/Join.js
+++ src/Join.js
@@ -7,6 +7,10 @@ const Join = () => {
   const {
     Id,
     IdError,
+    Nickname,
+    NicknameError,
+    Birth,
+    BirthError,
     email,
     emailError,
     password,
@@ -16,7 +20,9 @@ const Join = () => {
     handleIdChange,
     handleEmailChange,
     handlePasswordChange,
-    handlePasswordSame
+    handlePasswordSame,
+    handleNicknameChange,
+    handleBirthChange,
     } = useForm();
 
 
@@ -31,38 +37,37 @@ const Join = () => {
       <h2>회원 가입</h2>
 
       <div>
-        아이디 :
-        <input type="text" value={Id} onChange={handleIdChange} />
-        <p className={IdError ? 'errorMessage' : 'invisibleText'}>
-        {IdError ? '아이디는 5~15자리의 영문 대소문자와 숫자로만 입력해주세요.' : 'invisible text'}
-        </p>
+        <input type="text" placeholder='아이디' value={Id} onChange={handleIdChange} />
+        {IdError && <p className='errorMessage'> 아이디는 5~15자리의 영문 대소문자와 숫자로만 입력해주세요.</p>}
       </div>
 
       <div>
-        이메일 :
-        <input type="email" value={email} onChange={handleEmailChange} />
-        <p className={emailError ? 'errorMessage' : 'invisibleText'}>
-        {emailError ? '이메일 형식이 잘못되었습니다.' : 'invisible text'}
-        </p>
+        <input type="email" placeholder='이메일' value={email} onChange={handleEmailChange} />
+        {emailError && <p className='errorMessage'>이메일 형식이 잘못되었습니다.</p>}
       </div>
 
       <div>
-        비밀번호 :
-        <input type="password" value={password} onChange={handlePasswordChange} />
-        <p className={passwordError ? 'errorMessage' : 'invisibleText'}>
-        {passwordError ? '비밀번호는 8자리 이상이며 숫자, 대소문자 알파벳을 포함해야 합니다.' : 'invisible text'}
-        </p>
+        <input type="password" placeholder='비밀번호' value={password} onChange={handlePasswordChange} />
+        {passwordError && <p className='errorMessage'>비밀번호는 8자리 이상이며 숫자, 대소문자 알파벳을 포함해야 합니다.</p>}
       </div>
 
       <div>
-        비밀번호 확인 :
-        <input type="password" value={passwordCheck} onChange={handlePasswordSame} />
-        <p className={(!passwordError && passwordCheckError) ? 'errorMessage' : 'invisibleText'}>
-        {(!passwordError && passwordCheckError) ? '비밀번호가 다릅니다.' : 'invisible text'}
-        </p>
+        <input type="password" placeholder='비밀번호 확인' value={passwordCheck} onChange={handlePasswordSame} />
+
+        {(!passwordError && passwordCheckError) && <p className='errorMessage'>비밀번호가 다릅니다.</p>}
       </div>
 
-      <button type="submit" disabled={passwordError || emailError || IdError || passwordCheckError}>회원 가입</button>
+      <div>
+        <input type="text" placeholder='생년월일 8자' value={Birth} onChange={handleBirthChange} />
+        {BirthError && <p className='errorMessage'> 올바른 생년월일 8자를 입력해주세요</p>}
+      </div>
+
+      <div>
+        <input type="text" placeholder='닉네임' value={Nickname} onChange={handleNicknameChange} />
+        {NicknameError && <p className='errorMessage'> 닉네임을 2글자 이상 10 글자 이하로 입력해주세요.</p>}
+      </div>
+
+      <button type="submit" disabled={passwordError || emailError || IdError || passwordCheckError || !Id || NicknameError || BirthError }>회원 가입</button>
 
     </form>
   </div>
--- src/useForm.js
+++ src/useForm.js
@@ -10,6 +10,11 @@ export default function useForm() {
   const [passwordError, setPasswordError] = useState(false);
   const [passwordCheck, setPasswordCheck] = useState('');
   const [passwordCheckError, setPasswordCheckError] = useState(false);
+  const [Nickname, setNickname] = useState('');
+  const [NicknameError, setNicknameError] = useState(false);
+  const [Birth, setBirth] = useState('');
+  const [BirthError, setBirthError] = useState(false);
+
 
   const validatePassword = (password) => {
 	const re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
@@ -74,6 +79,84 @@ export default function useForm() {
     setPasswordCheck(newPass);
   }
 
+  const handleNicknameChange = (event) => {
+    const newNickname = event.target.value;
+
+    if (newNickname.length < 2 || newNickname.length > 10) {
+      setNicknameError(true);
+    } else {
+      setNicknameError(false);
+    }
+    setNickname(newNickname);
+  }
+
+  const validateBirth = (birth) => {
+    let flag = false;
+    let year;
+    let month;
+    let day;
+    if (birth.includes('.')) {
+      year = birth.slice(0, 4);
+      month = birth.slice(5, 7);
+      day = birth.slice(8, 10);
+      flag = /^\d{4}$/.test(year) && /^\d{2}$/.test(month) && /^\d{2}$/.test(day);
+    } else {
+      year = birth.slice(0, 4);
+      month = birth.slice(4, 6);
+      day = birth.slice(6, 8);
+      flag = /^\d{8}$/.test(birth);
+    }
+    if (parseInt(month) > 12 || parseInt(month) < 1) {
+      flag = false;
+    }
+    if (parseInt(day) > 31 || parseInt(day) < 1) {
+      flag = false;
+    }
+    if (parseInt(month) === 2 && parseInt(day) > 29) {
+      flag = false;
+    }
+    if (parseInt(month) === 4 && parseInt(day) > 30) {
+      flag = false;
+    }
+    if (parseInt(month) === 6 && parseInt(day) > 30) {
+      flag = false;
+    }
+    if (parseInt(month) === 9 && parseInt(day) > 30) {
+      flag = false;
+    }
+    if (parseInt(month) === 11 && parseInt(day) > 30) {
+      flag = false;
+    }
+    if (parseInt(year) % 4 !== 0 && parseInt(month) === 2 && parseInt(day) > 28) {
+      flag = false;
+    }
+    if (parseInt(year) % 100 === 0 && parseInt(year) % 400 !== 0 && parseInt(month) === 2 && parseInt(day) > 28) {
+      flag = false;
+    }
+    if (parseInt(year) < 1900 || parseInt(year) > new Date().getFullYear()) {
+      flag = false;
+    }
+    if (new Date(year, month - 1, day) > new Date()) {
+      flag = false;
+    }
+    return flag;
+  }
+
+  const handleBirthChange = (event) => {
+    const newBirth = event.target.value;
+
+    if (!validateBirth(newBirth)) {
+      setBirthError(true);
+    } else {
+      setBirthError(false);
+    }
+    if (newBirth.length === 8 && !newBirth.includes('.') && !isNaN(newBirth)) {
+      setBirth(`${newBirth.slice(0, 4)}.${newBirth.slice(4, 6)}.${newBirth.slice(6, 8)}`);
+    } else if (newBirth.length <= 10) {
+      setBirth(newBirth);
+    }
+  }
+
   return {
     Id,
     IdError,
@@ -86,6 +169,12 @@ export default function useForm() {
     handleIdChange,
     handleEmailChange,
     handlePasswordChange,
-    handlePasswordSame
+    handlePasswordSame,
+    Nickname,
+    NicknameError,
+    Birth,
+    BirthError,
+    handleNicknameChange,
+    handleBirthChange,
   }
 }
