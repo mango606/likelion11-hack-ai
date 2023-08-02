06de2e1bd3a3eac4aa0ed0b941e3baf00af61e8e
ychy61 <ychy010601@gmail.com>
2023-08-02 21:24:41 +0900

    [fix] 이미지 파일명, 클래스명 수정

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import React, { useState } from 'react';
+// import axios from 'axios';
 
 import './Login.css'
 
@@ -31,8 +32,26 @@ const Login = () =>{
                   console.log('로그인 실패');
                   setLoginError('아이디 또는 비밀번호를 잘못 입력했습니다.');
                 }
-
+                        // try {
+            // const response = await axios.post('서버', {
+            //       id: id,
+            //       password: password,
+            // });
+      
+            // if (response.data.success) {
+            //       console.log('로그인 성공');
+            //       setLoginError('');
+                  
+            // } else {
+            //       setLoginError('로그인 실패');
+            // }
+            // } catch (error) {
+            //       console.error('로그인 오류 : ', error);
+            //       setLoginError('로그인 실패');
+            // }
       };
+
+      //아이디랑 비밀번호 입력값을 지우는 함수
       const handleClearId = () => {
             setId('');
       };
@@ -58,7 +77,7 @@ const Login = () =>{
                         />
                         {/* 아이디 클리어 버튼 */}
                         {id && <div type="button" className="btn_clear" onClick={handleClearId}>
-                              <img className="btn_clear_img" alt="close_icon" src="img/close.png" />
+                              <img className="btn_clear_img" alt="clear_icon" src="img/clear.png" />
                               </div>}
                   </label>
                   {/* 비밀번호 input */}
@@ -71,8 +90,8 @@ const Login = () =>{
                               onChange={(e) => setPassword(e.target.value)} 
                         />
                         {/* 비밀번호 클리어 버튼 */}
-                        {password && <div type="button" className="login_clear" onClick={handleClearPassword}>
-                                          <img className="login_clear_img" alt="close_icon" src="img/clear.png" />
+                        {password && <div type="button" className="btn_clear" onClick={handleClearPassword}>
+                                          <img className="btn_clear_img" alt="clear_icon" src="img/clear.png" />
                                      </div>}
                   </label>
                   {/* 로그인에러메세지 */}
