70c3fae9718ab994c129aeebfb93f1e627662792
ychy61 <ychy010601@gmail.com>
2023-08-02 20:47:58 +0900

    [Feat] - 로그인 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import './App.css';
+import {Route, Routes, BrowserRouter} from "react-router-dom";
 
+import Main from "./pages/Main";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/"  element={<Main />} />
+          <Route path="/Login" element={<Login/>}/>
+        </Routes>
+      </div>
+    
+    </BrowserRouter>
 
-      </header>
-    </div>
   );
 }
 
