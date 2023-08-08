8837de21f9adf91529941a6a68d9ca5df8d3fa9f
kcwww <cwkim0321@gmail.com>
2023-08-08 19:53:26 +0900

    [fix] id -> username

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,93 +1,112 @@
 import { NavLink } from "react-router-dom";
-import React, { useState } from 'react';
-import './LoginPage.css';
+import React, { useState } from "react";
+import "./LoginPage.css";
+
+import axios from "axios";
 
 const LoginPage = () => {
   // 상태변수 선언
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   // const [loginSuccess, setLoginSuccess] = useState(false);
-  const [loginError, setLoginError] = useState('');
-  
+  const [loginError, setLoginError] = useState("");
 
   // 로그인 버튼 클릭시
   const handleLogin = async (e) => {
-        e.preventDefault();
+    e.preventDefault();
 
-        // 아이디 입력 안했을 때
-        if (id === '') {
-              setLoginError('아이디를 입력해주세요.');
-              return;
-        }
-        // 비밀번호 입력 안했을 때
-        else if (password === '') {
-              setLoginError('비밀번호를 입력해주세요.');
-              return;
-        }
 
-        console.log(`id: ${id}, Password: ${password}`);
-        if (id === '1234' && password === 'qwer') {
-              console.log('로그인 성공');
-              setLoginError('');
-            } else {
-              console.log('로그인 실패');
-              setLoginError('아이디 또는 비밀번호를 잘못 입력했습니다.');
-            }
-            
+    if (username === "") {
+      setLoginError("아이디를 입력해주세요.");
+      return;
+    }
+
+    else if (password === "") {
+      setLoginError("비밀번호를 입력해주세요.");
+      return;
+    }
+
+    try {
+      const response = await axios.post('/login', { username, password });
+      const token = response.data.token;
+      console.log(data);
+      localStorage.setItem('jwt', token);
+    } catch (error) {
+      console.error('로그인 실패', error);
+    }
   };
 
   //아이디랑 비밀번호 입력값을 지우는 함수
   const handleClearId = () => {
-        setId('');
+    setUsername("");
   };
   const handleClearPassword = () => {
-        setPassword('');
+    setPassword("");
   };
-  
 
-return (
-  <div className="container">
-         <NavLink to="/"><div className="title"> AILION </div></NavLink>
-        <form onSubmit={handleLogin}>
-              <label>
-                    <input 
-                          type="text"  
-                          placeholder="아이디" 
-                          id="username"
-                          value={id} 
-                          onChange={(e) => setId(e.target.value)} 
-                    />
-                    {id && <div type="button" className="btn_clear" onClick={handleClearId}>
-                          <img className="btn_clear_img" alt="close_icon" src="img/clear.png" />
-                          </div>}
-              </label>
-              <label>
-                    <input 
-                          type="password" 
-                          placeholder="비밀번호" 
-                          id="password" 
-                          value={password} 
-                          onChange={(e) => setPassword(e.target.value)} 
-                    />
-                    {password && <div type="button" className="btn_clear" onClick={handleClearPassword}>
-                                      <img className="btn_clear_img" alt="close_icon" src="img/clear.png" />
-                                 </div>}
-              </label>
-              {loginError && <div className="error_message">{loginError}</div>}
-              {loginError === '' && <div className="error_message"> </div>}
-              <button className="btn_login" type="submit">로그인</button>
-              <div className="txt_line"> 또는 </div>
-              <div className="btn_col">
-                    <button className="btn_naver">네이버 로그인</button>
-                    <button className="btn_kakao">Kakao 로그인</button>
-                    <button className="btn_Google">Google 로그인</button>
-              </div>
-              <NavLink to="/Join">
-                    <div className="link_join">회원가입</div>
-              </NavLink>             
-        </form>
-  </div>
-);
-}
+  return (
+    <div className="container">
+      <NavLink to="/">
+        <div className="title"> AILION </div>
+      </NavLink>
+      <form onSubmit={handleLogin}>
+        <label>
+          <input
+            type="text"
+            placeholder="아이디"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          {username && (
+            <div type="button" className="btn_clear" onClick={handleClearId}>
+              <img
+                className="btn_clear_img"
+                alt="close_icon"
+                src="img/clear.png"
+              />
+            </div>
+          )}
+        </label>
+        <label>
+          <input
+            type="password"
+            placeholder="비밀번호"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {password && (
+            <div
+              type="button"
+              className="btn_clear"
+              onClick={handleClearPassword}
+            >
+              <img
+                className="btn_clear_img"
+                alt="close_icon"
+                src="img/clear.png"
+              />
+            </div>
+          )}
+        </label>
+        {loginError && <div className="error_message">{loginError}</div>}
+        {loginError === "" && <div className="error_message"> </div>}
+        <button className="btn_login" type="submit">
+          로그인
+        </button>
+        <div className="txt_line"> 또는 </div>
+        <div className="btn_col">
+          <button className="btn_naver">네이버 로그인</button>
+          <button className="btn_kakao">Kakao 로그인</button>
+          <button className="btn_Google">Google 로그인</button>
+        </div>
+        <NavLink to="/Join">
+          <div className="link_join">회원가입</div>
+        </NavLink>
+      </form>
+    </div>
+  );
+};
 
 export default LoginPage;
