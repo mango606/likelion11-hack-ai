10a9fee83855611ddbfbc9789ebc778c6fed0455
acs0209 <annjae4676@naver.com>
2023-08-09 12:36:18 +0900

    [Feat] - 이미지 다운로드 구현

--- back-end/ailion/src/main/java/back/ailion/controller/AwsS3ApiController.java
+++ back-end/ailion/src/main/java/back/ailion/controller/AwsS3ApiController.java
@@ -1,13 +1,19 @@
 package back.ailion.controller;
 
+import back.ailion.model.dto.DownloadDto;
 import back.ailion.model.dto.FileUploadResponse;
 import back.ailion.model.dto.request.FileUploadRequest;
 import back.ailion.service.AwsS3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.List;
 
 @RestController
@@ -26,5 +32,15 @@ public class AwsS3ApiController {
         return awsS3Service.uploadFile(fileUploadRequest);
     }
 
+    // 이미지를 다운로드 받는 컨트롤러
+    @GetMapping("/image/download/{filename}")
+    public ResponseEntity<Resource> downloadImageFile(@PathVariable String filename) throws MalformedURLException {
 
+        DownloadDto downloadDto = awsS3Service.downloadImageFile(filename);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, downloadDto.getContentDisposition())
+                .body(downloadDto.getResource());
+
+    }
 }
--- back-end/ailion/src/main/java/back/ailion/service/AwsS3Service.java
+++ back-end/ailion/src/main/java/back/ailion/service/AwsS3Service.java
@@ -1,5 +1,6 @@
 package back.ailion.service;
 
+import back.ailion.model.dto.DownloadDto;
 import back.ailion.model.dto.FileUploadResponse;
 import back.ailion.model.dto.request.FileUploadRequest;
 import back.ailion.model.entity.FileUpload;
@@ -8,10 +9,14 @@ import back.ailion.model.entity.Post;
 import back.ailion.repository.ImageRepository;
 import back.ailion.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriUtils;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Service
@@ -46,5 +51,15 @@ public class AwsS3Service {
         String storeFileName = attachFile.getStoreFileName();
 
         return new FileUploadResponse(commonUtils.storeFile(multipartFile, storeFileName), fileUrls);
+    }
+
+    public DownloadDto downloadImageFile(String filename) throws MalformedURLException {
+
+        UrlResource resource = new UrlResource(commonUtils.getFullPath(filename));
+
+        String encodedUploadFileName = UriUtils.encode(filename, StandardCharsets.UTF_8);
+        String contentDisposition = "image; filename=\"" + encodedUploadFileName + "\"";
+
+        return new DownloadDto(contentDisposition, resource);
     }
 }
