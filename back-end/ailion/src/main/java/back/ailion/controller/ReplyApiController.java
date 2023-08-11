f6fb7a7d1bdc78ddaa43cfaa6696e5955f9138ee
acs0209 <annjae4676@naver.com>
2023-08-12 01:29:24 +0900

    [Feat] - 대댓글 관련 dto 예외처리 구현

--- back-end/ailion/src/main/java/back/ailion/controller/ReplyApiController.java
+++ back-end/ailion/src/main/java/back/ailion/controller/ReplyApiController.java
@@ -8,6 +8,8 @@ import back.ailion.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/reply")
 @RequiredArgsConstructor
@@ -16,19 +18,19 @@ public class ReplyApiController {
     private final ReplyService replyService;
 
     @PostMapping
-    public ReplyDto saveReply(@RequestBody ReplyRequestDto replyRequestDto) {
+    public ReplyDto saveReply(@Valid @RequestBody ReplyRequestDto replyRequestDto) {
 
         return replyService.saveReply(replyRequestDto);
     }
 
     @PatchMapping
-    public ReplyDto updateReply(@RequestBody ReplyUpdateDto replyUpdateDto) {
+    public ReplyDto updateReply(@Valid @RequestBody ReplyUpdateDto replyUpdateDto) {
 
         return replyService.updateReply(replyUpdateDto);
     }
 
     @DeleteMapping
-    public boolean deleteReply(@RequestBody ReplyDeleteDto replyDeleteDto) {
+    public boolean deleteReply(@Valid @RequestBody ReplyDeleteDto replyDeleteDto) {
 
         return replyService.deleteReply(replyDeleteDto);
     }
--- back-end/ailion/src/main/java/back/ailion/model/dto/request/ReplyDeleteDto.java
+++ back-end/ailion/src/main/java/back/ailion/model/dto/request/ReplyDeleteDto.java
@@ -4,11 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Positive;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReplyDeleteDto {
 
+    @Positive
     private Long replyId;
+
+    @Positive
     private Long postId;
 }
