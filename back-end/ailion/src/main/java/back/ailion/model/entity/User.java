1245ad858416696ea76b8d23094bd90a3394697a
HanYoonSoo <hdbstn3055@gmail.com>
2023-08-13 21:58:38 +0900

    MyPage Repository

--- back-end/ailion/src/main/java/back/ailion/model/entity/User.java
+++ back-end/ailion/src/main/java/back/ailion/model/entity/User.java
@@ -55,6 +55,9 @@ public class User extends BaseEntity {
     @OneToMany(mappedBy = "user")
     private List<Heart> hearts = new ArrayList<>();
 
+    @OneToMany(mappedBy = "user")
+    private List<Favorite> favorites = new ArrayList<>();
+
     @ManyToMany
     @JoinTable(
             name = "user_authority",
