d15ca13c9187c27ae524506db35ea8c5205d5497
HanYoonSoo <hdbstn3055@gmail.com>
2023-08-17 16:07:54 +0900

    AWS Version

--- back-end/ailion/src/main/java/back/ailion/config/WebMvcConfig.java
+++ back-end/ailion/src/main/java/back/ailion/config/WebMvcConfig.java
@@ -57,10 +57,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("/*")    //외부에서 들어오는 모둔 url 을 허용
+                .allowedOrigins("/**")    //외부에서 들어오는 모둔 url 을 허용
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")    //허용되는 Method
                 .allowedHeaders("*")    //허용되는 헤더
-                .allowCredentials(true);    //자격증명 허용
+                .allowCredentials(false);    //자격증명 허용
     }
 
     @Override
