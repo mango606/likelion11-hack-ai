4b92af67bb4acb6eaf2fce53b200c1beb62c6e3e
mango606 <yuj08184@kangwon.ac.kr>
2023-08-18 12:01:11 +0900

    [Style] 홈 화면 추가

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div>
+            <p id="home-title">AI, 어느 정도 아시나요?</p>
+            <p id="home-subtitle">AI에 대한 정보 얻고 돈 벌기, AI 커뮤니티에서 함께 시작하세요!</p>
+            <button id="home-btn">이용방법</button>
+        </div>
+    );
+}
+  
+export default Home;
